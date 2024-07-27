ddb61d8a5a8802b11e59e5d751a46dcb051cd063
Yuchan Han <serltretu24@gmail.com>
2024-07-27 10:49:54 +0900

    feat: user router > authorization url 생성

--- router/user.py
+++ router/user.py
@@ -1,12 +1,45 @@
-from fastapi import APIRouter, Depends
+import os
+import uuid
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+
+from fastapi import APIRouter, HTTPException, Depends, Request
+from fastapi.responses import RedirectResponse
 from fastapi_restful.cbv import cbv
 from passlib.context import CryptContext
 
-router = APIRouter()
+from entity.user import User as ODMUser
+from aiogoogle import Aiogoogle, auth as aiogoogle_auth
+import aiogoogle
+from service.credential import depends_credential, Credential, get_current_user
+
+load_dotenv(verbose=True)
+router = APIRouter(prefix="/user", tags=["user"])
+
+GOOGLE_CLIENT_CREDS = aiogoogle_auth.creds.ClientCreds(
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    scopes=["profile", "email"],
+    redirect_uri="http://localhost:8000/user/authorization_url",
+)
+GOOGLE_STATE = uuid.uuid4().hex
 
 
 @cbv(router)
-class Home:
-    @router.get("/home")
-    async def root(self):
-        return {"message": "Hello Home"}
+class User:
+    password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    google_oauth = Aiogoogle(
+        client_creds=GOOGLE_CLIENT_CREDS,
+    )
+
+    @router.get("/authorization_url")
+    async def get_authorization_url(self):
+        return RedirectResponse(
+            url=self.google_oauth.oauth2.authorization_url(
+                client_creds=GOOGLE_CLIENT_CREDS,
+                state=GOOGLE_STATE,
+                access_type="online",
+                include_granted_scopes=True,
+                prompt="select_account",
+            )
+        )
