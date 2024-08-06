086c92252879f5c2c4eb3cf56aa802e589284f57
Yuchan Han <serltretu24@gmail.com>
2024-08-07 00:37:13 +0900

    fix: flow model field name rename

--- entity/flow.py
+++ entity/flow.py
@@ -1,36 +1,40 @@
 from beanie import Document
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from typing import List, Optional, Dict
 
+
 class NodeData(BaseModel):
-    inputContent: Optional[str] = None
+    content: Optional[str] = None
     generated: bool = False
-    promptAddress: Optional[str] = None
+    prompt_address: Optional[str] = None
     label: Optional[str] = None
-    aiGenerated: Optional[bool] = None
+    ai_generated: Optional[bool] = None
     request: Optional[Dict] = None
+
 
 class Node(BaseModel):
     id: str
-    type: str
+    node_type: str
     position: Dict[str, int]
     data: NodeData
 
+
 class Edge(BaseModel):
     id: str
-    type: str
+    edge_type: str
     source: str
     target: str
+
 
 class EditorOption(BaseModel):
     viewport: Dict[str, int]
 
+
 class Flow(Document):
-    editorOption: EditorOption
+    editor_option: EditorOption
     nodes: List[Node]
     edges: List[Edge]
     is_community: bool = False
-
 
     class Settings:
         name = "flows"  # MongoDB 컬렉션 이름
