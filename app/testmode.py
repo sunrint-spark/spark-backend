aa058b20c691410360cdd84386e191bb954c43b8
Yuchan Han <serltretu24@gmail.com>
2024-08-07 00:17:20 +0900

    feat: testmode 기능 추가

---
+++ app/testmode.py
@@ -0,0 +1,31 @@
+import os
+import logging
+from dotenv import load_dotenv
+from utils.log import Logger
+from entity.user import User as ODMUser
+from service.credential import Credential
+
+logger = Logger.create(__name__, level=logging.DEBUG)
+load_dotenv()
+
+if os.getenv("TESTMODE") == "true":
+    logger.info("Test Mode On")
+    TEST_USER_ACCESS_TOKEN = "eySPARKTESTAAAAAA" + os.urandom(6).hex() + "BK00000000007218"
+    SPARK_TEST_USER_PROFILE = ("https://lh3.googleusercontent.com/a"
+                               "/ACg8ocKJXng59DxsYIIwhbuRCDTQjh0YBseiGNxu1urEuPJmBtBUk3hc=s96-c")
+    logger.debug(f"Test User Access Token: " + TEST_USER_ACCESS_TOKEN)
+
+
+async def get_or_create_test_user() -> "ODMUser":
+    odm_user = await ODMUser.find({"email": "SPARK.TESTUSER@internal.temp"}).first_or_none()
+    if not odm_user:
+        odm_user = ODMUser(
+            name="Spark Kim",
+            username="testuser.internal",
+            email="SPARK.TESTUSER@internal.temp",
+            profile_url=SPARK_TEST_USER_PROFILE,
+        )
+        odm_user = await odm_user.create()
+    return odm_user
+
+
