790a2f3ada1671d54b4357802a8d027546dc2804
sh-owo <sngho0718@gmail.com>
2024-08-10 18:43:35 +0900

    feat: 질문 다양성 증가

--- router/gpt.py
+++ router/gpt.py
@@ -12,7 +12,19 @@ google_api_key = os.getenv("GOOGLE_API_KEY")
 google_search_engine_id = os.getenv("GOOGLE_SEARCH_ENGINE_ID")
 engine_name = "gpt-4o-mini-2024-07-18"
 
-system_message = """만약 어떤 단어나 문장 뒤에 키워드 라는 말이 있으면 그 말의 키워드를 3~4개를 keyword:"" json형식으로 출력하고 "status":select"로 출력해야해 예를 들면
+system_message = """만약 어떤 단어나 문장 뒤에 생성과 비슷한 의미의 시작 단어가 있으면 그 말의 키워드를 3~4를 keyword:"" json 형식으로 출력하고 "status":"start"로 출력해야해
+                                자율주행차 시작을 입력헀을때
+                                {
+                                    "keyword": [
+                                        "자율주행 기술",
+                                        "센서 및 데이터 처리",
+                                        "교통안전",
+                                        "법규 및 정책"
+                                    ],
+                                    "status": "start"
+                                }
+                                
+                                만약 어떤 단어나 문장 뒤에 키워드 라는 말이 있으면 그 말의 키워드를 3~4개를 keyword:"" json형식으로 출력하고 "status":select"로 출력해야해 예를 들면
                                 자율주행차 키워드를 입력헀을때
                                 {
                                     "keyword": [
@@ -24,39 +36,9 @@ system_message = """만약 어떤 단어나 문장 뒤에 키워드 라는 말�
                                     "status": "select"
                                 }
                                 또 사용자가 키워드를 입력하면 그 키워드에 대해 좀 더 자세한 키워드를 3~4씩 출력해야해
-                                만약 문장 뒤에 다시 라는 말이 있으면  그 말의 키워드를 새롭게 3~4개를 keyword:"" json 형식으로 출력하고 status는 방금 전 대화의 status로 출력해야해 예를 들면
-                                 자율주행차 키워드를 입력헀을때
-                                {
-                                    "keyword": [
-                                        "자율주행 기술",
-                                        "센서 및 데이터 처리",
-                                        "교통안전",
-                                        "법규 및 정책"
-                                    ],
-                                    "status": "select"
-                                }
-                                자율주행차 다시 를 입력헀을때
-                                {
-                                    "keyword": [
-                                        "자율주행 기술",
-                                        "센서 및 데이터 처리",
-                                        "인공지능",
-                                        "자율주행차 시장 동향"
-                                    ],
-                                    "status": "select"
-                                }
-                                만약 어떤 단어나 문장 뒤에 생성과 비슷한 의미의 시작 단어가 있으면 그 말의 키워드를 3~4를 keyword:"" json 형식으로 출력하고 "status":"start"로 출력해야해
-                                자율주행차 시작을 입력헀을때
-                                {
-                                    "keyword": [
-                                        "자율주행 기술",
-                                        "센서 및 데이터 처리",
-                                        "교통안전",
-                                        "법규 및 정책"
-                                    ],
-                                    "status": "start"
-                                }
-
+                                
+                                만약 문장 뒤에 다시 라는 말이 있으면 그 말의 키워드를 새롭게 3~4개를 keyword:"" json 형식으로 출력하고 status는 방금 전 대화의 status로 출력해야해
+                                
                                 또 만약 어떤 단어나 문장 뒤에 마무리 혹은 까지 라는 말이 있으면 그 말의 키워드를 3~4개를  keyword:"" json 형태로 출력하고 그 말을 설명한 문장을 description:""에 저장하고 
                                 "status":"end"로 출력해야해 또한 마지막에 가장 최근인 status:start인 것부터 지금까지의 대화를 간단한 문장으로 정리해 image_keyword에 저장해야해 예를들면  
                                 {
@@ -108,13 +90,14 @@ class GPT:
                     "content": prompt
                 }
             ],
-            temperature=0.3,
+            temperature=0.7,
             max_tokens=1000,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
             # response_format={"type": "json_object"}
         )
+
         response = json.loads(completion.choices[0].message.content)
         image_query = response.get("image_keyword")
 
@@ -124,7 +107,8 @@ class GPT:
             response["image_urls"] = image_urls
             print("이미지 검색 완료!")
 
-        print(f'image_query: {image_query}')
+
+
         print(f'input: {prompt}')
         print(f'response: {response}')
         return response
