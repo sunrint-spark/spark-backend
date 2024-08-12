321b777a73c3643888f7ee9ddcf692f0cea1f27d
sh-owo <sngho0718@gmail.com>
2024-08-12 23:24:21 +0900

    feat:node

---
+++ router/node.py
@@ -0,0 +1,31 @@
+import logging
+from fastapi import APIRouter, Depends, HTTPException, status, Request
+from fastapi_restful.cbv import cbv
+from entity.flow import Node as FlowNode
+from entity.user import User as ODMUser
+from service.credential import get_current_user
+from utils.log import Logger
+
+logger = Logger.create(__name__, level=logging.DEBUG)
+
+router = APIRouter(
+    prefix="/save",
+    tags=["Node"],
+)
+
+@cbv(router)
+class Node:
+    @router.get("/node")
+    async def get_node(self):
+        return {"message": "Hello Node"}
+
+    @router.post("/node", response_model=FlowNode, status_code=status.HTTP_201_CREATED)
+    async def create_node(
+        self, node: FlowNode, user: ODMUser = Depends(get_current_user),
+    ):
+        try:
+            await node.create()
+            return node
+        except Exception as e:
+            logger.error(f"Error creating node[user:{user.id}]: {e}")
+            raise HTTPException(status_code=500, detail="Internal Server Error")
