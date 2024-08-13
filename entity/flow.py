a08210558b2fbd6de8f54a9e2bfd1198196e261a
Yuchan Han <serltretu24@gmail.com>
2024-08-13 12:03:56 +0900

    refactor: flow 모델 구조 수정

--- entity/flow.py
+++ entity/flow.py
@@ -1,6 +1,9 @@
 from beanie import Document
-from pydantic import BaseModel
-from typing import List, Optional, Dict
+from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
+from typing import List, Optional, Dict, Literal, NewType
+
+UserId = NewType("UserId", str)
 
 
 class NodeData(BaseModel):
@@ -30,11 +33,16 @@ class EditorOption(BaseModel):
     viewport: Dict[str, int]
 
 
+class FlowUserPermission(BaseModel):
+    permission: list[Literal["read", "write"]]
+
+
 class Flow(Document):
-    editor_option: EditorOption
-    nodes: List[Node]
-    edges: List[Edge]
-    is_community: bool = False
+    id: UUID = Field(default_factory=uuid4, alias="_id")
+    editor_option: Dict[UserId, EditorOption] = Field(default={})
+    nodes: List[Node] = Field(default=[])
+    edges: List[Edge] = Field(default=[])
+    permission: Dict[UserId, List[FlowUserPermission]] = Field(default=[])
 
     class Settings:
         name = "flows"  # MongoDB 컬렉션 이름
