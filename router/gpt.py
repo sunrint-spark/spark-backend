d5d058d2ad4568debc6b9371419d1f644db868c2
sh-owo <sngho0718@gmail.com>
2024-08-15 03:19:06 +0900

    feat: notion_log.py service로 이동

--- router/gpt.py
+++ router/gpt.py
@@ -7,7 +7,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from .notion_log import notionlog
+from service.notion_log import notionlog
 
 router = APIRouter(
     tags=["GPT"],
