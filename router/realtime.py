a94bdcc78aa6cd2dafc4ab2cfd25471ea83dfc5f
Yuchan Han <serltretu24@gmail.com>
2024-08-15 02:30:19 +0900

    fix: realtime 버그 수정
    진짜 정상작동함 와!

--- router/realtime.py
+++ router/realtime.py
@@ -1,8 +1,8 @@
+import logging, asyncio
 from fastapi import (
     APIRouter,
     HTTPException,
     Depends,
-    Body,
     WebSocket,
     WebSocketDisconnect,
     BackgroundTasks,
@@ -20,6 +20,9 @@ from service.realtime import (
 )
 from service.websocket import ConnectionManager, depends_websocket_manager
 from utils.flow import exist_node_in, update_dict_in_list, exist_edge_in
+
+from utils.log import Logger
+logger = Logger.create(__name__, level=logging.DEBUG)
 
 router = APIRouter(prefix="/realtime", tags=["realtime"])
 
@@ -47,13 +50,10 @@ class Realtime:
         self.connections = connections
 
     @staticmethod
-    @router.post("/join")
+    @router.post("/join/{flow_id}")
     async def join_realtime(
+        flow_id: str,
         current_user: ODMUser = Depends(get_current_user),
-        flow_id: str = Body(
-            title="Flow ID",
-            description="Realtime 방에 참가할 Flow ID",
-        ),
         rt_flow: RealtimeFlowSession = Depends(depends_flow_realtime),
         rt_user: RealtimeUserSession = Depends(depends_user_realtime),
         connections: ConnectionManager = Depends(depends_websocket_manager),
@@ -62,41 +62,42 @@ class Realtime:
         return await realtime._join_realtime(current_user, flow_id)
 
     async def _join_realtime(self, current_user: ODMUser, flow_id: str):
-        odm_flow = await ODMFlow.find({"id": flow_id}).first_or_none()
+        odm_flow = await ODMFlow.get(flow_id)
         if odm_flow is None:
             raise HTTPException(status_code=404, detail="Flow not found")
         if not odm_flow.permission.get(str(current_user.id)):
             raise HTTPException(status_code=403, detail="Permission Denied")
         if not await self.rt_flow.exists(flow_id):
+            print(odm_flow.model_dump())
             await self.rt_flow.update(flow_id, odm_flow.model_dump())
             user_realtime_data = {
-                current_user.id: {
+                str(current_user.id): {
                     "database": current_user.model_dump(),
                     "position": {
                         "x": 0,
                         "y": 0,
                     },
-                    "chat": None,
+                    "chat": "",
                 }
             }
             await self.rt_user.update(flow_id, user_realtime_data)
         else:
             user_realtime_data = await self.rt_user.get(flow_id)
-            if current_user.id not in user_realtime_data:
-                user_realtime_data[current_user.id] = {
+            if str(current_user.id) not in user_realtime_data:
+                user_realtime_data[str(current_user.id)] = {
                     "database": current_user.model_dump(),
                     "position": {
                         "x": 0,
                         "y": 0,
                     },
-                    "chat": None,
+                    "chat": "",
                 }
                 await self.rt_user.update(flow_id, user_realtime_data)
-        await self.worker_join_user(flow_id, current_user)
 
         export_data = odm_flow.model_dump()
         del export_data["permission"]
         del export_data["editor_option"]
+        logger.info(f"User {current_user.username} joined realtime session({flow_id})")
         return {
             "message": "Joined Realtime Session",
             "data": export_data,
@@ -105,24 +106,30 @@ class Realtime:
     async def websocket_bg_worker(
         self, flow_id: str, odm_user: "ODMUser", worker_data: dict
     ):
+        logger.info(f"User {odm_user.name}({str(odm_user.id)}) send data: {worker_data}")
         if str(worker_data["op"]) == "3":
+            logger.info(f"User {odm_user.name}({str(odm_user.id)}) send mouse move {worker_data['data']}")
             await self.worker_mouse_move(flow_id, odm_user, worker_data["data"])
             mouse_move_data = await self.rt_user.get(flow_id)
-            mouse_move_data[odm_user.id]["position"] = worker_data["data"]
+            mouse_move_data[str(odm_user.id)]["position"] = worker_data["data"]
             await self.rt_user.update(flow_id, mouse_move_data)
             # position: {"x": 0, "y": 0}
         elif str(worker_data["op"]) == "4":
+            logger.info(f"User {odm_user.name}({str(odm_user.id)}) change node {worker_data['data']}")
             await self.worker_change_node(flow_id, odm_user, worker_data["data"])
             await self.node_redis_update(flow_id, worker_data["data"])
         elif str(worker_data["op"]) == "5":
+            logger.info(f"User {odm_user.name}({str(odm_user.id)}) change edge {worker_data['data']}")
             await self.worker_change_edge(flow_id, odm_user, worker_data["data"])
             await self.edge_redis_update(flow_id, worker_data["data"])
         elif str(worker_data["op"]) == "6":
+            logger.info(f"User {odm_user.name}({str(odm_user.id)}) send chat {worker_data['data']['message']}")
             await self.worker_user_chat(
                 flow_id, odm_user, worker_data["data"]["message"]
             )
         else:
             ws_user_connection = self.connections.get(flow_id, str(odm_user.id))
+            logger.info(f"User {odm_user.name}({str(odm_user.id)}) send invalid operation {worker_data}")
             await ws_user_connection.send_json(
                 {
                     "op": -3,
@@ -136,9 +143,10 @@ class Realtime:
     async def node_redis_update(self, flow_id: str, update_node_data: dict):
         flow_data = await self.rt_flow.get(flow_id)
         node_id: str = update_node_data["id"]
+        logger.info(f"Redis Flow Data edit requested: {flow_id} {update_node_data}")
         if not exist_node_in(flow_data, node_id):
             flow_data["nodes"].append(update_node_data)
-            return
+            return await self.rt_flow.update(flow_id, flow_data)
 
         if update_node_data.get("data"):
             flow_data["nodes"] = update_dict_in_list(
@@ -176,7 +184,7 @@ class Realtime:
             {
                 "op": 1,
                 "data": {
-                    "uid": odm_user.id,
+                    "uid": str(odm_user.id),
                     "name": odm_user.username,
                     "username": odm_user.username,
                     "profile_url": odm_user.profile_url,
@@ -190,7 +198,7 @@ class Realtime:
             {
                 "op": 2,
                 "data": {
-                    "uid": odm_user.id,
+                    "uid": str(odm_user.id),
                     "username": odm_user.username,
                     "profile_url": odm_user.profile_url,
                 },
@@ -214,7 +222,7 @@ class Realtime:
             {
                 "op": 3,
                 "data": {
-                    "uid": odm_user.id,
+                    "uid": str(odm_user.id),
                     "position": position,
                 },
             },
@@ -228,7 +236,7 @@ class Realtime:
             {
                 "op": 4,
                 "data": {
-                    "uid": odm_user.id,
+                    "uid": str(odm_user.id),
                     "node": node_data,
                 },
             },
@@ -242,7 +250,7 @@ class Realtime:
             {
                 "op": 5,
                 "data": {
-                    "uid": odm_user.id,
+                    "uid": str(odm_user.id),
                     "flow": flow_data,
                 },
             },
@@ -256,7 +264,7 @@ class Realtime:
             {
                 "op": 6,
                 "data": {
-                    "uid": odm_user.id,
+                    "uid": str(odm_user.id),
                     "message": message,
                 },
             },
@@ -279,24 +287,40 @@ async def realtime_endpoint(
     if current_user is None:
         return await websocket.close()
     realtime_instance = Realtime(rt_flow, rt_user, connections)
-    odm_flow = await ODMFlow.find({"id": flow_id}).first_or_none()
+    odm_flow = await ODMFlow.get(flow_id)
     if odm_flow is None:
         await websocket.send_json({"op": -3, "msg": "Flow not found"})
         return await websocket.close()
-    session_users = await rt_user.get(flow_id)
-    if not session_users or current_user.id not in session_users:
+    try:
+        session_users = await rt_user.get(flow_id)
+    except TypeError:
+        await websocket.send_json({"op": -3, "msg": "Session not found"})
+        return await websocket.close()
+    if not session_users or str(current_user.id) not in session_users.keys():
         await websocket.send_json({"op": -3, "msg": "Flow not found"})
         return await websocket.close()
     await connections.connect(flow_id, str(current_user.id), websocket)
+    await realtime_instance.worker_join_user(flow_id, current_user)
+    is_in_recent = False
+    for recent_flows in current_user.recent:
+        if recent_flows["id"] == flow_id:
+            is_in_recent = True
+    if not is_in_recent:
+        current_user.recent.append({"id": flow_id, "name": odm_flow.name})
+        await current_user.save()
     try:
         await websocket.send_json({"op": 0, "msg": "Connected"})
         while True:
             received_data = await websocket.receive_json()
+            logger.info(f"User {current_user.name}({str(current_user.id)}) send data: {received_data}")
             background_tasks.add_task(
                 realtime_instance.websocket_bg_worker,
-                flow_id,
-                current_user,
-                received_data,
+                flow_id=flow_id,
+                odm_user=current_user,
+                worker_data=received_data,
+            )
+            await asyncio.create_task(
+                realtime_instance.websocket_bg_worker(flow_id, current_user, received_data)
             )
     except WebSocketDisconnect:
         await realtime_instance.worker_leave_user(flow_id, current_user)
