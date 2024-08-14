109e285e5618415a1bcc55d5598db04ddb581cc4
Yuchan Han <serltretu24@gmail.com>
2024-08-14 14:57:08 +0900

    fix: flow router

--- entity/flow.py
+++ entity/flow.py
@@ -4,19 +4,35 @@ from uuid import UUID, uuid4
 from typing import List, Dict, Literal
 
 
+class Node(BaseModel):
+    id: str
+    type: str
+    position: Dict[str, int]
+    measured: Dict[str, int] | dict
+    data: dict
+    selected: bool
+    dragging: bool
+
+
+class Edge(BaseModel):
+    id: str
+    source: str
+    target: str
+
+
 class EditorOption(BaseModel):
     viewport: Dict[str, int]
 
 
 class FlowUserPermission(BaseModel):
-    permission: list[Literal["read", "write"]]
+    permission: list[Literal["read", "write", "owner"]]
 
 
 class Flow(Document):
     id: UUID = Field(default_factory=uuid4, alias="_id")
     editor_option: Dict[str, EditorOption] = Field(default={})
-    nodes: List[dict] = Field(default=[])
-    edges: List[dict] = Field(default=[])
+    nodes: List[Node] = Field(default=[])
+    edges: List[Edge] = Field(default=[])
     permission: Dict[str, List[FlowUserPermission]] = Field(default=[])
 
     class Settings:
--- router/flow.py
+++ router/flow.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Request
 from fastapi_restful.cbv import cbv
 from beanie import PydanticObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
-from entity.flow import Flow
+from entity.flow import Flow as ODMFlow, FlowUserPermission, Node, Edge, EditorOption
 from entity.user import User as ODMUser
 from service.credential import get_current_user
 from utils.log import Logger
@@ -18,16 +18,75 @@ router = APIRouter(
 
 @cbv(router)
 class Flow:
-    @router.post("/", response_model=Flow, status_code=status.HTTP_201_CREATED)
+    @router.post("/", status_code=status.HTTP_201_CREATED)
     async def create_flow(
         self,
-        flow: Flow,
+        prompt: str,
         user: "ODMUser" = Depends(get_current_user),
     ):
-        try:
-            await flow.create()
-            return flow
-        except Exception as e:
-            # 예외 처리 및 로깅
-            logger.error(f"Error creating flow[user:{user.id}]: {e}")
-            raise HTTPException(status_code=500, detail="Internal Server Error")
+        create_flow_model = ODMFlow(
+            permission={str(user.id): FlowUserPermission(permission=["owner"])},
+            nodes=[
+                Node(
+                    id="system@start",
+                    type="aiStartupNode",
+                    measured={},
+                    position={"x": 0, "y": 0},
+                    data={
+                        "text": prompt,
+                    },
+                    selected=True,
+                    dragging=True,
+                )
+            ],
+            edges=[],
+            editor_option={
+                str(user.id): EditorOption(viewport={"x": 0, "y": 0}),
+            },
+        )
+        await create_flow_model.insert()
+        return {"message": "Create flow", "data": {"id": str(create_flow_model.id)}}
+
+    @router.get("/recommend")
+    async def get_recommend_prompt(
+        self,
+        _user: "ODMUser" = Depends(get_current_user),
+    ):
+        return {
+            "message": "Recommend prompt",
+            "data": [
+                "What is the best way to learn programming?",
+                "What is the best way to learn programming?",
+                "What is the best way to learn programming?",
+            ],
+        }
+
+    @router.get("/")
+    async def get_project_flows(
+        self,
+        _user: "ODMUser" = Depends(get_current_user),
+    ):
+        return {
+            "message": "Project flows",
+            "data": [
+                {
+                    "id": "flow_id",
+                    "name": "flow_name",
+                }
+            ],
+        }
+
+    @router.get("/recent")
+    async def get_recent_flows(
+        self,
+        _user: "ODMUser" = Depends(get_current_user),
+    ):
+        return {
+            "message": "Recent flows",
+            "data": [
+                {
+                    "id": "flow_id",
+                    "name": "flow_name",
+                }
+            ],
+        }
