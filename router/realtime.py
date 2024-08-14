3320e66118831e0fedab6dd8446a1323abc705c0
Yuchan Han <serltretu24@gmail.com>
2024-08-14 14:56:22 +0900

    feat: realtime (+support credential with ws)

--- router/realtime.py
+++ router/realtime.py
@@ -1,4 +1,3 @@
-from fastapi_restful.cbv import cbv
 from fastapi import (
     APIRouter,
     HTTPException,
@@ -7,12 +6,11 @@ from fastapi import (
     WebSocket,
     WebSocketDisconnect,
     BackgroundTasks,
+    Query,
 )
-
 from entity.user import User as ODMUser
 from entity.flow import Flow as ODMFlow
-from entity.internal.frontend import FrontendModal, FrontendAction
-from service.credential import get_current_user
+from service.credential import get_current_user, get_current_user_ws
 
 from service.realtime import (
     depends_flow_realtime,
@@ -23,42 +21,86 @@ from service.realtime import (
 from service.websocket import ConnectionManager, depends_websocket_manager
 from utils.flow import exist_node_in, update_dict_in_list, exist_edge_in
 
-router = APIRouter(
-    prefix="/realtime",
-    tags=["realtime"],
-)
+router = APIRouter(prefix="/realtime", tags=["realtime"])
 
 
-@cbv(router)
+@router.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        print(f"Client {client_id} disconnected")
+
+
 class Realtime:
-    rt_flow: RealtimeFlowSession = Depends(depends_flow_realtime)
-    rt_user: RealtimeUserSession = Depends(depends_user_realtime)
-    connections: ConnectionManager = Depends(depends_websocket_manager)
-
-    @router.websocket("/{flow_id}")
-    async def realtime_endpoint(
+    def __init__(
         self,
-        websocket: WebSocket,
-        background_tasks: BackgroundTasks,
-        flow_id: str,
-        current_user: "ODMUser" = Depends(get_current_user),
+        rt_flow: RealtimeFlowSession,
+        rt_user: RealtimeUserSession,
+        connections: ConnectionManager,
     ):
+        self.rt_flow = rt_flow
+        self.rt_user = rt_user
+        self.connections = connections
+
+    @staticmethod
+    @router.post("/join")
+    async def join_realtime(
+        current_user: ODMUser = Depends(get_current_user),
+        flow_id: str = Body(
+            title="Flow ID",
+            description="Realtime 방에 참가할 Flow ID",
+        ),
+        rt_flow: RealtimeFlowSession = Depends(depends_flow_realtime),
+        rt_user: RealtimeUserSession = Depends(depends_user_realtime),
+        connections: ConnectionManager = Depends(depends_websocket_manager),
+    ):
+        realtime = Realtime(rt_flow, rt_user, connections)
+        return await realtime._join_realtime(current_user, flow_id)
+
+    async def _join_realtime(self, current_user: ODMUser, flow_id: str):
         odm_flow = await ODMFlow.find({"id": flow_id}).first_or_none()
         if odm_flow is None:
             raise HTTPException(status_code=404, detail="Flow not found")
-        session_users = await self.rt_user.get(flow_id)
-        if not session_users or current_user.id not in session_users:
+        if not odm_flow.permission.get(str(current_user.id)):
             raise HTTPException(status_code=403, detail="Permission Denied")
+        if not await self.rt_flow.exists(flow_id):
+            await self.rt_flow.update(flow_id, odm_flow.model_dump())
+            user_realtime_data = {
+                current_user.id: {
+                    "database": current_user.model_dump(),
+                    "position": {
+                        "x": 0,
+                        "y": 0,
+                    },
+                    "chat": None,
+                }
+            }
+            await self.rt_user.update(flow_id, user_realtime_data)
+        else:
+            user_realtime_data = await self.rt_user.get(flow_id)
+            if current_user.id not in user_realtime_data:
+                user_realtime_data[current_user.id] = {
+                    "database": current_user.model_dump(),
+                    "position": {
+                        "x": 0,
+                        "y": 0,
+                    },
+                    "chat": None,
+                }
+                await self.rt_user.update(flow_id, user_realtime_data)
+        await self.worker_join_user(flow_id, current_user)
 
-        await self.connections.connect(flow_id, str(current_user.id), websocket)
-        try:
-            await websocket.send_json({"op": 0})
-            while True:
-                received_data = await websocket.receive_json()
-                background_tasks.add_task(self.websocket_bg_worker, flow_id, current_user, received_data)
-        except WebSocketDisconnect:
-            await self.worker_leave_user(flow_id, current_user)
-            self.connections.disconnect(flow_id, str(current_user.id))
+        export_data = odm_flow.model_dump()
+        del export_data["permission"]
+        del export_data["editor_option"]
+        return {
+            "message": "Joined Realtime Session",
+            "data": export_data,
+        }
 
     async def websocket_bg_worker(
         self, flow_id: str, odm_user: "ODMUser", worker_data: dict
@@ -76,14 +118,19 @@ class Realtime:
             await self.worker_change_edge(flow_id, odm_user, worker_data["data"])
             await self.edge_redis_update(flow_id, worker_data["data"])
         elif str(worker_data["op"]) == "6":
-            await self.worker_user_chat(flow_id, odm_user, worker_data["data"]["message"])
+            await self.worker_user_chat(
+                flow_id, odm_user, worker_data["data"]["message"]
+            )
         else:
             ws_user_connection = self.connections.get(flow_id, str(odm_user.id))
             await ws_user_connection.send_json(
-                {"op": -3, "data": {
-                    "message": "Invalid Operation",
-                    "error": "websocket.invalid.opcode"
-                }}
+                {
+                    "op": -3,
+                    "data": {
+                        "message": "Invalid Operation",
+                        "error": "websocket.invalid.opcode",
+                    },
+                }
             )
 
     async def node_redis_update(self, flow_id: str, update_node_data: dict):
@@ -215,51 +262,42 @@ class Realtime:
             },
         )
 
-    @router.post("/join")
-    async def join_realtime(
-        self,
-        current_user: "ODMUser" = Depends(get_current_user),
-        flow_id: str = Body(
-            title="Flow ID",
-            description="Realtime 방에 참가할 Flow ID",
-        ),
-    ):
-        odm_flow = await ODMFlow.find({"id": flow_id}).first_or_none()
-        if odm_flow is None:
-            raise HTTPException(status_code=404, detail="Flow not found")
-        if not odm_flow.permission.get(str(current_user.id)):
-            raise HTTPException(status_code=403, detail="Permission Denied")
-        if not await self.rt_flow.exists(flow_id):
-            await self.rt_flow.update(flow_id, odm_flow.model_dump())
-            user_realtime_data = {
-                current_user.id: {
-                    "database": current_user.model_dump(),
-                    "position": {
-                        "x": 0,
-                        "y": 0,
-                    },
-                    "chat": None,
-                }
-            }
-            await self.rt_user.update(flow_id, user_realtime_data)
-        else:
-            user_realtime_data = await self.rt_user.get(flow_id)
-            if current_user.id not in user_realtime_data:
-                user_realtime_data[current_user.id] = {
-                    "database": current_user.model_dump(),
-                    "position": {
-                        "x": 0,
-                        "y": 0,
-                    },
-                    "chat": None,
-                }
-                await self.rt_user.update(flow_id, user_realtime_data)
-        await self.worker_join_user(flow_id, current_user)
 
-        export_data = odm_flow.model_dump()
-        del export_data["permission"]
-        del export_data["editor_option"]
-        return {
-            "message": "Joined Realtime Session",
-            "data": export_data,
-        }
+@router.websocket("/{flow_id}")
+async def realtime_endpoint(
+    websocket: WebSocket,
+    background_tasks: BackgroundTasks,
+    flow_id: str,
+    token: str = Query(...),
+    rt_flow: RealtimeFlowSession = Depends(depends_flow_realtime),
+    rt_user: RealtimeUserSession = Depends(depends_user_realtime),
+    connections: ConnectionManager = Depends(depends_websocket_manager),
+):
+    print("hello")
+    await websocket.accept()
+    current_user = await get_current_user_ws(token, websocket)
+    if current_user is None:
+        return await websocket.close()
+    realtime_instance = Realtime(rt_flow, rt_user, connections)
+    odm_flow = await ODMFlow.find({"id": flow_id}).first_or_none()
+    if odm_flow is None:
+        await websocket.send_json({"op": -3, "msg": "Flow not found"})
+        return await websocket.close()
+    session_users = await rt_user.get(flow_id)
+    if not session_users or current_user.id not in session_users:
+        await websocket.send_json({"op": -3, "msg": "Flow not found"})
+        return await websocket.close()
+    await connections.connect(flow_id, str(current_user.id), websocket)
+    try:
+        await websocket.send_json({"op": 0, "msg": "Connected"})
+        while True:
+            received_data = await websocket.receive_json()
+            background_tasks.add_task(
+                realtime_instance.websocket_bg_worker,
+                flow_id,
+                current_user,
+                received_data,
+            )
+    except WebSocketDisconnect:
+        await realtime_instance.worker_leave_user(flow_id, current_user)
+        connections.disconnect(flow_id, str(current_user.id))
--- service/websocket.py
+++ service/websocket.py
@@ -3,29 +3,43 @@ from service.credential import get_redis_pool
 
 
 class ConnectionManager:
-    def __init__(self):
-        self.active_connections: dict[str, dict[str, WebSocket]] = {}
-        self.redis_connection = get_redis_pool()
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(ConnectionManager, cls).__new__(cls)
+            cls._instance.active_connections = {}
+            cls._instance.redis_connection = get_redis_pool()
+        return cls._instance
 
     async def connect(self, flow_id: str, user_id: str, websocket: WebSocket):
-        await websocket.accept()
-        if not self.active_connections.get(flow_id):
+        if flow_id not in self.active_connections:
             self.active_connections[flow_id] = {}
         self.active_connections[flow_id][user_id] = websocket
 
     def disconnect(self, flow_id: str, user_id: str):
-        del self.active_connections[flow_id][user_id]
+        if (
+            flow_id in self.active_connections
+            and user_id in self.active_connections[flow_id]
+        ):
+            del self.active_connections[flow_id][user_id]
+            if not self.active_connections[flow_id]:
+                del self.active_connections[flow_id]
 
     def get(self, flow_id: str, user_id: str) -> WebSocket:
-        return self.active_connections[flow_id][user_id]
+        return self.active_connections.get(flow_id, {}).get(user_id)
 
     async def broadcast(self, flow_id: str, data: dict):
-        for connection in self.active_connections[flow_id].values():
-            try:
-                await connection.send_json(data)
-            except Exception as e:
-                pass
+        if flow_id in self.active_connections:
+            for connection in self.active_connections[flow_id].values():
+                try:
+                    await connection.send_json(data)
+                except Exception as e:
+                    print(f"Broadcasting error: {e}")
 
 
-async def depends_websocket_manager():
-    return ConnectionManager()
+connection_manager = ConnectionManager()
+
+
+def depends_websocket_manager():
+    return connection_manager
